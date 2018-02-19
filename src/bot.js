bccc894fad390bb9050bfcd2cd0822aac81684e1
knize <kirill.knize@gmail.com>
2018-02-20 00:09:55 +0300

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,38 @@
+const token = '510256795:AAFNtQ5iwkpAvOfPnCVSbOIqlc0CIh_xGoQ';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.post('/new-message', function (req, res) {
+    const {message} = req.body
+
+
+    if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
+        return res.end()
+    }
+
+    axios.post('https://api.telegram.org/bot' + token + '/sendMessage', {
+        chat_id: message.chat.id,
+        text: 'Polo!!'
+    })
+        .then(response => {
+            console.log('Message posted')
+            res.end('ok')
+        })
+        .catch(err => {
+            console.log('Error :', err)
+            res.end('Error :' + err)
+        })
+
+});
+
+app.listen(3000, function () {
+    console.log('We are up!');
+});
