cd0f68e1f37a0e2ee71c877d92953cea68fe8c06
knize <kirill.knize@gmail.com>
2018-02-20 06:55:44 +0300

    set inline_keyboard in reply markup

--- src/bot.js
+++ src/bot.js
@@ -71,7 +71,12 @@ function initButtons() {
     ];
 }
 
-const inline_keyboard = initButtons();
+const keyboard = initButtons();
+
+const reply_markup = {
+    inline_keyboard: keyboard,
+    resize_keyboard: true
+};
 
 
 app.post('/new-message', function (req, res) {
@@ -89,7 +94,7 @@ app.post('/new-message', function (req, res) {
         console.log('Message: ' + message.text);
         if (message.text === '/start') {
             console.log('Start');
-            sendMessage(message.chat.id, '0', inline_keyboard, res);
+            sendMessage(message.chat.id, '0', reply_markup, res);
         }
     }
     res.end('ok');
@@ -100,7 +105,7 @@ app.listen(process.env.PORT, function () {
 });
 
 
-function sendMessage(chatId, text, inline_keyboard, res) {
+function sendMessage(chatId, text, reply_markup = null, res) {
     axios.post('https://api.telegram.org/bot' + telegram_token + '/sendMessage', {
         chat_id: chatId,
         text: text,
