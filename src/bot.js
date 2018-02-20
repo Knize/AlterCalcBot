2eb31fe3950aff4aa43df8744917076d61bb9b2d
knize <kirill.knize@gmail.com>
2018-02-20 03:21:20 +0300

    reply markup first

--- src/bot.js
+++ src/bot.js
@@ -9,6 +9,33 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+const keyboard = [
+    ['AC', '+', '-'],
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    [' ', '0', ' '],
+];
+
+const reply_markup = {
+    keyboard: keyboard,
+    resize_keyboard: true,
+    one_time_keyboard: true
+};
+
+axios.post('https://api.telegram.org/bot' + telegram_token + '/sendMessage', {
+    chat_id: message.chat.id,
+    text: 'Polo!!',
+    reply_markup: reply_markup
+})
+    .then(response => {
+        console.log('Message posted');
+    })
+    .catch(err => {
+        console.log('Error :', err);
+    });
+
+
 app.post('/new-message', function (req, res) {
     const {message} = req.body;
     console.log('Request processing start.');
@@ -21,7 +48,7 @@ app.post('/new-message', function (req, res) {
     console.log('Sending response');
     axios.post('https://api.telegram.org/bot' + telegram_token + '/sendMessage', {
         chat_id: message.chat.id,
-        text: 'Polo!!'
+        text: 'Polo!!',
     })
         .then(response => {
             console.log('Message posted');
@@ -36,3 +63,5 @@ app.post('/new-message', function (req, res) {
 app.listen(process.env.PORT, function () {
     console.log('We are up!');
 });
+
+
