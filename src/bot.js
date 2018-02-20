5cfa16b08109f8cedece6b2ce27263330b41c666
knize <kirill.knize@gmail.com>
2018-02-20 03:30:10 +0300

    add debug to /start flow

--- src/bot.js
+++ src/bot.js
@@ -24,7 +24,6 @@ const reply_markup = {
 };
 
 
-
 app.post('/new-message', function (req, res) {
     const {message} = req.body;
     console.log('Request processing start.');
@@ -34,7 +33,8 @@ app.post('/new-message', function (req, res) {
         return res.end()
     }
 
-    if(message === '/start'){
+    console.log('Message: ' + message);
+    if (message === '/start') {
         console.log('Start');
         axios.post('https://api.telegram.org/bot' + telegram_token + '/sendMessage', {
             chat_id: message.chat.id,
