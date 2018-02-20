a66acb6f7da123d6b602dbc2d8ccc1861d3c0bf8
knize <kirill.knize@gmail.com>
2018-02-20 18:59:42 +0300

    add padding

--- src/bot.js
+++ src/bot.js
@@ -80,10 +80,11 @@ app.post('/new-message', function (req, res) {
         const data = callback_query.data;
         const chat_id = callback_query.message.chat.id;
         const result = processAction(oldText, data, chat_id);
+        const paddedResult = result.padStart(15);
         if (result !== NOTHING_CHANGED) {
-            editMessageText(callback_query, result, res);
+            editMessageText(callback_query, paddedResult, res);
         }
-        answerCallbackQuery(callback_query.id, result, res);
+        answerCallbackQuery(callback_query.id, paddedResult, res);
     }
     res.end('ok');
 });
