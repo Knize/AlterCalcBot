ce22fe006988e0326a11ca02e327de9fe2403b5e
knize <kirill.knize@gmail.com>
2018-02-20 06:56:50 +0300

    update processing flow fix

--- src/bot.js
+++ src/bot.js
@@ -84,19 +84,19 @@ app.post('/new-message', function (req, res) {
     const {callback_query} = req.body;
     const {message} = req.body;
     console.log(req.body);
-    if (callback_query != null) {
-        const {callback_query} = req.body;
-        console.log('Callback processing start.');
-        console.log('Callback: ' + callback_query.id + '; Inline message id: ' + callback_query.inline_message_id);
-        answerCallbackQuery(callback_query, '', false, res);
-    }
     if (message != null) {
         console.log('Message: ' + message.text);
         if (message.text === '/start') {
             console.log('Start');
             sendMessage(message.chat.id, '0', reply_markup, res);
         }
+    } else if (callback_query != null) {
+        const {callback_query} = req.body;
+        console.log('Callback processing start.');
+        console.log('Callback: ' + callback_query.id + '; Inline message id: ' + callback_query.inline_message_id);
+        answerCallbackQuery(callback_query, '', false, res);
     }
+
     res.end('ok');
 });
 
