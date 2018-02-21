bb6ad3a674c32b5678c407277718be85239440bc
knize <kirill.knize@gmail.com>
2018-02-21 07:24:44 +0300

    eliminate leading 0 in right operand

--- src/bot.js
+++ src/bot.js
@@ -183,6 +183,7 @@ function newProcessAction(expression, action, chat_id) {
             console.log("case: default");
             if (expression === '0' && action === '0') return NOTHING_CHANGED;
             if (expression === '0') return action;
+            if(lastIsZero(expression)) return expression.slice(0, expression.length - 1) + action;
             return expression + action;
     }
 }
@@ -193,6 +194,10 @@ function isOperator(action) {
 
 function lastIsOperator(expression) {
     return isOperator(expression.slice(-1));
+}
+
+function lastIsZero(expression) {
+    return expression.slice(-1) === '0';
 }
 
 function isNumber(expression) {
